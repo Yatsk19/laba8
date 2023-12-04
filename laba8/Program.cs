366ed71c24ecde442eb44563370c26c4278c02dc
Vetallll19 <144685380+Yatsk19@users.noreply.github.com>
2023-12-04 11:07:56 +0200

    first commit

---
+++ laba8/Program.cs
@@ -0,0 +1,9 @@
+﻿Singleton патернуclass ConfigurationManager:
+    _instance = None
+    def __new__(cls):        if cls._instance is None:
+            cls._instance = super(ConfigurationManager, cls).__new__(cls)            cls._instance.config = { }
+return cls._instance
+    def get_configuration(self, key):        return self.config.get(key)
+    def set_configuration(self, key, value):
+        self.config[key] = value
+    def save_configuration(self):        print("Configuration saved successfully.")
---
+++ laba81/Program.cs
@@ -0,0 +1,17 @@
+﻿class Graph(ABC) : @abstractmethod
+    def draw(self):        pass
+
+class LineGraph(Graph) : def draw(self):
+        print("Drawing Line Graph")
+class BarGraph(Graph) : def draw(self):
+        print("Drawing Bar Graph")
+class PieChart(Graph) : def draw(self):
+        print("Drawing Pie Chart")
+class GraphFactory(ABC) : @abstractmethod
+    def create_graph(self):        pass
+class LineGraphFactory(GraphFactory) :
+    def create_graph(self):        return LineGraph()
+class BarGraphFactory(GraphFactory) :
+    def create_graph(self):        return BarGraph()
+class PieChartFactory(GraphFactory) :
+    def create_graph(self):        return PieChart()
